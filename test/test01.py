141577adf9f20a9641c39d00641b3f40258cf108
atiroms <atirom.umusus@gmail.com>
2022-03-30 23:29:21 +0900

    naomichi implementation

--- test/test01.py
+++ test/test01.py
@@ -3,10 +3,12 @@ from pulp import *
 from ortoolpy import addvars, addbinvars
 from io import StringIO
 
-p_in = 'D:/NICT_WS/Dropbox/dutyshift/refs/dr_fujikawa/night01.csv'
-
+#p_in = 'D:/NICT_WS/Dropbox/dutyshift/refs/dr_fujikawa/night01.csv'
+p_in = 'D:/atiro/Dropbox/dutyshift/test/night01.csv'
 d_in = pd.read_csv(p_in)
 d_in = d_in.set_index('dr')
 d_in = d_in.T
+
+d_shift = pd.DataFrame(np.ndarray())
 
 model = LpProblem()
