b90389abea2a3146bc7d2d805c20656e394fc093
atiroms <atirom.umusus@gmail.com>
2022-04-01 16:35:51 +0900

    assignment count limit implemented

--- test/test01.py
+++ test/test01.py
@@ -1,12 +1,16 @@
 import numpy as np, pandas as pd
+import os
 from pulp import *
 from ortoolpy import addvars, addbinvars
 from io import StringIO
 
+#p_test = 'D:/NICT_WS/Dropbox/dutyshift/test'
+p_test = 'D:/atiro/Dropbox/dutyshift/test'
+
+c_outlier = 1.0
+
 # Data of Dr availability
-p_availability = 'D:/NICT_WS/Dropbox/dutyshift/test/night01.csv'
-#p_in = 'D:/atiro/Dropbox/dutyshift/test/night01.csv'
-d_availability = pd.read_csv(p_availability)
+d_availability = pd.read_csv(os.path.join(p_test, 'night01.csv'))
 d_availability = d_availability.set_index('dr')
 d_availability = d_availability.T
 
@@ -16,41 +20,49 @@ n_dr = len(l_dr)
 n_date = len(l_date)
 
 # Data of assignment count limits
-p_count = 'D:/NICT_WS/Dropbox/dutyshift/test/membertest01.csv'
-d_count = pd.read_csv(p_count)
+d_count = pd.read_csv(os.path.join(p_test, 'membertest01.csv'))
 d_count = d_count.set_index('dr')
 
 # Binary assignment variables to be optimized
 dv_assign = pd.DataFrame(np.array(addbinvars(n_date, n_dr)), columns = l_dr, index = l_date)
 
 # Model to be optimized
-m = LpProblem()
+problem = LpProblem()
 
-# Iterate over days
-for (_, r_availability),(_, r_assign) in zip(d_availability.iterrows(),dv_assign.iterrows()):
-    r_ng = (r_availability == 0)
-    # One member per day
-    m += (lpSum(r_assign) == 1)
-    # Do not assign to a day if not available
-    m += (lpDot(r_ng, r_assign) <= 0)
+# Iterate over date
+for date in l_date:
+    s_availability = d_availability.loc[date]
+    s_ng = (s_availability == 0)
+    sv_assign = dv_assign.loc[date]
+    # One member per date
+    problem += (lpSum(sv_assign) == 1)
+    # Do not assign to a date if not available
+    problem += (lpDot(s_ng, sv_assign) <= 0)
 
-v_excess = pd.Series(np.array(addvars(n_dr)), index = l_dr)
-v_shortage = pd.Series(np.array(addvars(n_dr)), index = l_dr)
-
+# v_outlier represents excess from max or shortage from min in the shape of '\__/
 # Iterate over members
+v_outlier = pd.Series(np.array(addvars(n_dr)), index = l_dr)
 for dr in l_dr:
-    # Excess of assignment count
-    m += (v_excess[dr] == d_count.loc[dr, 'max'] - lpSum(dv_assign[dr]))
-    # Shortage of assignment count
-    m += (v_shortage[dr] == lpSum(dv_assign[dr]) - d_count.loc[dr, 'min'])
-    # Excess of assignment count must be 0
-    m += (v_excess[dr] == 0)
-    # Shortage of assignment count must be 0
-    m += (v_shortage[dr] == 0)
+    problem += (v_outlier[dr] >= lpSum(dv_assign[dr]) - d_count.loc[dr, 'max'])
+    problem += (v_outlier[dr] >= d_count.loc[dr, 'min'] - lpSum(dv_assign[dr]))
+    problem += (v_outlier[dr] >= 0)
 
+# Minimize outlier
+problem += c_outlier * lpSum(v_outlier)
+
+# Print problem
+#print('Problem')
+#print(problem)
 
 # Optimize model
-m.solve()
+problem.solve()
+
+# Print solution status
+print('Status')
+print(LpStatus[problem.status])
+# Problem function value
+v_objective = value(problem.objective)
+print(v_objective)
 
 # Extract values from variable
 d_result = pd.DataFrame(np.vectorize(value)(dv_assign), columns = l_dr, index = l_date)
