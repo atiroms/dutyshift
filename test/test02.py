e8715e9d58a5e04b2d72585026a66e837f8f84d0
atiroms <atirom.umusus@gmail.com>
2022-04-07 00:06:26 +0900

    date_duty to type_duty

--- test/test02.py
+++ test/test02.py
@@ -6,6 +6,9 @@ from ortoolpy import addvars, addbinvars
 #p_test = 'D:/NICT_WS/Dropbox/dutyshift/test'
 p_test = 'D:/atiro/Dropbox/dutyshift/test'
 
+l_date_em = [6, 20]
+l_date_ect = [4, 6, 7, 11, 13, 14, 18, 20, 21, 25, 27, 28]
+l_type_duty = ['night_tot','night_em','night_wd','night_hd','day_hd','oc_tot','oc_hd_day','oc_other','ect']
 c_outlier = 1.0
 min_interval = 7
 c_continuity = 1.0
@@ -16,12 +19,12 @@ d_member = pd.read_csv(os.path.join(p_test, 'member02.csv'))
 d_member_idx = d_member[['id_dr','name_jpn','title_jpn','designation_jpn','ect_asgn_jpn','name','title_short','designation']]
 s_ect_asgn = d_member['ect_asgn']
 s_designation = d_member['designation']
-d_limits = d_member[['night_tot','night_em','night_wd','night_hd','day_hd','oc_tot','oc_hd_day','oc_other','ect']]
+d_limits = d_member[l_type_duty]
 
 # Split assignment limit data into hard and soft
 d_limits_hard = pd.DataFrame([[[np.nan]*2]*d_limits.shape[1]]*d_limits.shape[0], index = d_limits.index, columns = d_limits.columns)
 d_limits_soft = pd.DataFrame([[[np.nan]*2]*d_limits.shape[1]]*d_limits.shape[0], index = d_limits.index, columns = d_limits.columns)
-for col in d_limits.columns:
+for col in l_type_duty:
     for idx in d_limits.index:
         if '(' in d_limits.loc[idx, col]:
             # If parenthesis exists, its content is hard limit
@@ -34,12 +37,13 @@ for col in d_limits.columns:
 
         for d_temp in [d_limits_hard, d_limits_soft]:
             if d_temp.loc[idx, col][0] == '-':
-                # Convert '-' to np.nan
-                d_temp.loc[idx, col] = [np.nan, np.nan]
+                # Convert '-' to [np.nan, np.nan]
+                d_temp.loc[idx, col] = [np.nan]*2
             elif '-' in str(d_temp.loc[idx, col][0]):
                 # Convert string 'a-b' to list [a, b]
                 d_temp.loc[idx, col] = [int(x) for x in str(d_temp.loc[idx, col][0]).split('-')]
             else:
+                # Convert string 'a' to list [a, a]
                 d_temp.loc[idx, col] = [int(d_temp.loc[idx, col][0])]*2
 
 # Data of Dr availability
@@ -49,24 +53,54 @@ d_availability.set_index('id_dr', inplace=True)
 d_availability.drop(['name_jpn'], axis = 1, inplace = True)
 d_availability = d_availability.T
 
+# Dr, date and duty lists and dataframes
 l_dr = d_availability.columns.to_list()
-l_date = d_availability.index.to_list()
 n_dr = len(l_dr)
+d_date_duty = pd.DataFrame([[date_duty] for date_duty in d_availability.index.to_list()], columns = ['date_duty'])
+d_date_duty['date'] = d_date_duty['date_duty'].apply(lambda x: int(x.split('_')[0]))
+d_date_duty['duty'] = d_date_duty['date_duty'].apply(lambda x: x.split('_')[1])
+d_date_duty_ect = pd.DataFrame(zip([str(date)+'_ect' for date in l_date_ect], l_date_ect, ['ect']*len(l_date_ect)), columns = ['date_duty', 'date', 'duty'])
+d_date_duty = pd.concat([d_date_duty, d_date_duty_ect], axis = 0)
+l_date = sorted([date for date in d_date_duty['date'].unique().tolist()])
+l_duty = ['day', 'night', 'am', 'pm', 'oc', 'ect']
+l_date_hd = sorted(d_date_duty.loc[d_date_duty['duty'] == 'day', 'date'].unique().tolist())
+l_date_wd = [date for date in l_date if date not in l_date_hd]
 n_date = len(l_date)
+n_duty = len(l_duty)
+n_duty_date = d_date_duty.shape[0]
 
-# Data of assignment count limits
-d_count = pd.read_csv(os.path.join(p_test, 'membertest01.csv'))
-d_count = d_count.set_index('dr')
+# Specify which type_duty's apply to each date_duty
+d_date_duty['type_duty_apl'] = [[]] * n_duty_date
+# night_tot
+for date in l_date:
+    v_in = d_date_duty.loc[(d_date_duty['date'] == date) & (d_date_duty['duty'] == 'night'), 'type_duty_apl'].to_list()[0]
+    v_out = v_in + ['night_tot']
+    d_date_duty.loc[(d_date_duty['date'] == date) & (d_date_duty['duty'] == 'night'), 'type_duty_apl'] = [v_out]
+# night_em
+for date in l_date_em:
+    v_in = d_date_duty.loc[(d_date_duty['date'] == date) & (d_date_duty['duty'] == 'night'), 'type_duty_apl'].to_list()[0]
+    v_out = v_in + ['night_em']
+    d_date_duty.loc[(d_date_duty['date'] == date) & (d_date_duty['duty'] == 'night'), 'type_duty_apl'] = [v_out]
+# night_wd
+for date in l_date_wd:
+    v_in = d_date_duty.loc[(d_date_duty['date'] == date) & (d_date_duty['duty'] == 'night'), 'type_duty_apl'].to_list()[0]
+    v_out = v_in + ['night_wd']
+    d_date_duty.loc[(d_date_duty['date'] == date) & (d_date_duty['duty'] == 'night'), 'type_duty_apl'] = [v_out]
+
 
 # Binary assignment variables to be optimized
-dv_assign = pd.DataFrame(np.array(addbinvars(n_date, n_dr)), columns = l_dr, index = l_date)
+dv_assign = pd.DataFrame(np.array(addbinvars(n_duty_date, n_dr)), columns = l_dr, index = d_date_duty['date_duty'].to_list())
 
-# Model to be optimized
+# Initialize model to be optimized
 problem = LpProblem()
 
-# Assign one Dr per date
-for date in l_date:
-    problem += (lpSum(dv_assign.loc[date]) == 1)
+# Assign one Dr per date_duty for ['night', 'day', 'am', 'pm']
+for duty in ['night', 'day', 'am', 'pm']:
+    for duty_date in d_date_duty[d_date_duty['duty'] == duty]['date_duty'].to_list():
+        problem += (lpSum(dv_assign.loc[duty_date]) == 1)
+
+# 
+
 
 # Do not assign to a date if not available
 problem += (lpDot((d_availability == 0).to_numpy(), dv_assign.to_numpy()) <= 0)
@@ -75,7 +109,7 @@ problem += (lpDot((d_availability == 0).to_numpy(), dv_assign.to_numpy()) <= 0)
 dv_assign_suboptimal = lpDot((d_availability == 1).to_numpy(), dv_assign.to_numpy())
 
 # Minimize outlier
-# Penalize excess from max or shortage from min in the shape of '\__/
+# Penalize excess from max or shortage from min in the shape of '\__/'
 sv_outlier = pd.Series(np.array(addvars(n_dr)), index = l_dr)
 for dr in l_dr:
     # Define sv_outlier for each dr
