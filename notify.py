346ddf199ea20eccd644f7efb9cb784722e144bd
atiroms <atirom.umusus@gmail.com>
2023-05-26 00:36:25 +0900

    misc

--- notify.py
+++ notify.py
@@ -17,7 +17,7 @@ from googleapiclient.errors import HttpError
 # Parameters
 ###############################################################################
 year_plan = 2023
-month_plan = 5
+month_plan = 6
 
 t_sleep = 600
 
