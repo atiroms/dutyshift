3c3ea307cb2e78c541a135fbdd2afd89869b157b
smrt <atirom.umusus@gmail.com>
2024-03-29 15:32:30 +0900

    misc

--- check.py
+++ check.py
@@ -3,6 +3,30 @@ import pandas as pd
 import os
 from helper import *
 
+def check_availability_all(lp_root, year_plan, month_plan):
+    p_root, p_month, p_data = prep_dirs(lp_root, year_plan, month_plan, prefix_dir = '', make_data_dir = False)
+
+    d_member = pd.read_csv(os.path.join(p_month, 'member.csv'), index_col = 0)
+    d_availability = pd.read_csv(os.path.join(p_month, 'availability.csv'))
+    d_availability.index = d_availability['date_duty']
+    d_availability = d_availability.iloc[:,1:]
+    d_availability = d_availability.T
+
+    d_availability_member = pd.DataFrame(index = d_availability.columns.tolist())
+
+    for date_duty in d_availability.columns:
+        l_member_available = d_availability.loc[d_availability[date_duty] > 0, :].index.tolist()
+        str_member_available = ', '.join(l_member_available)
+        l_member_available_jpn = [d_member.loc[d_member['id_member'] == int(member), 'name_jpn'].tolist()[0] for member in l_member_available]
+        str_member_available_jpn = ', '.join(l_member_available_jpn)
+        l_member_available_mail = [d_member.loc[d_member['id_member'] == int(member), 'email'].tolist()[0] for member in l_member_available]
+        str_member_available_mail = ', '.join(l_member_available_mail)
+        d_availability_member.loc[date_duty, 'l_member'] = str_member_available
+        d_availability_member.loc[date_duty, 'l_member_jpn'] = str_member_available_jpn
+        d_availability_member.loc[date_duty, 'l_member_mail'] = str_member_available_mail
+
+    return(d_availability_member)
+
 def check_availability_date_duty(lp_root, year_plan, month_plan, date_duty):
     p_root, p_month, p_data = prep_dirs(lp_root, year_plan, month_plan, prefix_dir = '', make_data_dir = False)
 
---
+++ parameter.py
@@ -0,0 +1,54 @@
+
+###############################################################################
+# Fixed parameters
+###############################################################################
+# General
+year_start, month_start = 2024, 4
+lp_root = ['/home/atiroms/Documents','D:/atiro','D:/NICT_WS','/Users/smrt']
+# Form
+dict_duty = {'ect': 0, 'am': 1, 'pm': 2, 'day': 3, 'ocday': 4, 'night': 5, 'emnight':6, 'ocnight': 7}
+dict_duty_jpn = {'am': '午前日直', 'pm': '午後日直', 'day': '日直', 'ocday': '日直OC', 'night': '当直', 'emnight': '救急当直', 'ocnight': '当直OC'}
+dict_jpnday = {0: '月', 1: '火', 2: '水', 3: '木', 4: '金', 5: '土', 6: '日'}
+#dict_duty_jpn = {'am': '午前日直', 'pm': '午後日直', 'day': '日直', 'ocday': '日直OC', 'night': '当直', 'ocnight': '当直OC'}
+dict_score_duty = {'duty':         ['am', 'pm', 'day', 'night', 'emnight', 'ocday', 'ocnight', 'ect'],
+                   'ampm':         [0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                   'daynight':     [0.0, 0.0, 1.0, 1.0, 1.5, 0.0, 0.0, 0.0],
+                   'ampmdaynight': [0.5, 0.5, 1.0, 1.0, 1.5, 0.0, 0.0, 0.0],
+                   'oc':           [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0],
+                   'ect':          [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]}
+dict_title_duty = {'assoc':            ['ocday', 'ocnight'],
+                   'instr':            ['am', 'pm', 'ocday', 'ocnight'],
+                   'assist_leader':    ['am', 'pm', 'day', 'night', 'ocday', 'ocnight'],
+                   'assist_subleader': ['am', 'pm', 'day', 'night'],
+                   'limtermclin':      ['am', 'pm', 'day', 'night'],
+                   'stud':             ['day', 'night'],
+                   'assist_child':     ['am', 'pm']}
+dict_class_duty = {'class': ['ampm', 'ampm', 'daynight_tot', 'daynight_tot', 'daynight_tot', 'night_em', 'night_wd', 'night_wd', 'daynight_hd', 'daynight_hd', 'oc_tot', 'oc_tot', 'oc_day', 'oc_night', 'ect'],
+                   'date':  ['all', 'all', 'all', 'all', 'all', 'all', 'wd', 'all', 'all', 'hd', 'all', 'all', 'all', 'all', 'all'],
+                   'duty':  ['am', 'pm', 'day', 'night', 'emnight', 'emnight', 'night', 'emnight', 'day', 'night', 'ocday', 'ocnight', 'ocday', 'ocnight', 'ect']}
+# Data collection
+address_response = "https://docs.google.com/spreadsheets/d/1zCdtuSHwzYn4idJM6ZZzzLJbQh4cJjf8uL8b4FySVCw/edit?resourcekey#gid=1443213949"
+# Optimizing assignment count
+l_day_ect = [0, 2, 3] # Monday, Wednesday, Thursday
+day_em, l_week_em = 2, [] # Wednesday, 1st and 3rd weeks
+l_type_score = ['ampm','daynight','ampmdaynight','oc','ect']
+l_class_duty = ['ampm','daynight_tot','night_em','night_wd','daynight_hd','oc_tot','oc_day','oc_night','ect']
+dict_score_class = {'score': ['ampm', 'daynight', 'daynight', 'ampmdaynight', 'ampmdaynight', 'ampmdaynight', 'oc', 'ect'],
+                    'class': ['ampm', 'daynight_tot', 'night_em', 'ampm', 'daynight_tot', 'night_em', 'oc_tot', 'ect'],
+                    'constant': [0.5, 1, 0.5, 0.5, 1, 0.5, 1, 1]}
+# Optimizing assignment, parameters for avoiding/penalizing close duties
+dict_closeduty = {'daynight': {'l_duty': ['day', 'ocday', 'night', 'emnight', 'ocnight'], 'thr_hard': 1, 'thr_soft': 5},
+                  'ect':      {'l_duty': ['ect'],                                         'thr_hard': 1, 'thr_soft': 4},
+                  'ampm':     {'l_duty': ['am', 'pm'],                                    'thr_hard': 1, 'thr_soft': 5}}
+# Optimizing assignment, parameters for avoiding overlapping duties
+ll_avoid_adjacent = [[['pm', 0], ['night', 0], ['emnight', 0], ['ocnight', 0]], [['night', 0], ['emnight', 0], ['ocnight', 0], ['ect', 1], ['am', 1]]]
+l_title_fulltime = ['assist'] # ['limterm_instr', 'assist', 'limterm_clin']
+# Notification
+t_sleep = 600
+dict_time_duty = {'duty': ['am', 'pm', 'day', 'night', 'emnight', 'ocday', 'ocnight', 'ect'],
+                  'duty_jpn': ['午前日直', '午後日直', '日直', '当直', '救急当直', '日直OC', '当直OC', 'ECT当番'],
+                  'start': ['08:30', '12:30', '08:30', '17:15', '17:15', '08:30', '17:15', '07:30'],
+                  'end': ['12:30', '17:15', '17:15', '32:30', '32:30', '17:15', '32:30', '11:00']}
+# Parameters for replacement
+sheet_id = "1glzf0fM1jyAZffFE7l7SHE26m3M4QBI5AAOsdSlmHxE"
+l_scope = ['https://www.googleapis.com/auth/calendar']
